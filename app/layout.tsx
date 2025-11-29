b16d75a121bf4926a86920db67e2b65e4dc806a1
Mørke Drevos <drtseo@proton.me>
2025-11-29 11:42:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-import './globals.css';
-import type { ReactNode } from 'react';
-import { Providers } from './providers';
+import './globals.css'
+import type { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
