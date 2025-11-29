5e98cc83c7530d9cf3ce206a4c95b1c5b169f8d7
Mørke Drevos <drtseo@proton.me>
2025-11-29 12:20:39 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { ReactNode } from 'react'
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
