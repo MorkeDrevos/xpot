d0233c4bf41b245b42b098e8fbf29edb92376a73
Mørke Drevos <drtseo@proton.me>
2025-11-30 18:47:30 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
+// app/dashboard/page.tsx
 'use client';
+
 export const dynamic = 'force-dynamic';
 
 import Link from 'next/link';
@@ -7,7 +9,7 @@ import { useEffect, useState } from 'react';
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { WalletReadyState } from '@solana/wallet-adapter-base'; // 👈 ADD THIS
+import { WalletReadyState } from '@solana/wallet-adapter-base';
 import { useSession } from 'next-auth/react';
 
 import { REQUIRED_XPOT } from '../../lib/xpot';
