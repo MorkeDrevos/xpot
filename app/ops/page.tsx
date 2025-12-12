d09a1284a3b77e1148c61c82c65605950adb398f
Mørke Drevos <drtseo@proton.me>
2025-12-12 20:51:14 +0100

    Create page.tsx

---
+++ app/ops/page.tsx
@@ -0,0 +1,9 @@
+// app/admin/page.tsx
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
+import AdminClient from './AdminClient';
+
+export default function AdminPage() {
+  return <AdminClient />;
+}
