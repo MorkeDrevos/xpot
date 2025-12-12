dcad64193ffc72f792c43327203a66059ae204cf
Mørke Drevos <drtseo@proton.me>
2025-12-12 20:51:24 +0100

    Delete app/admin/page.tsx

--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/admin/page.tsx
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
-
-import AdminClient from './AdminClient';
-
-export default function AdminPage() {
-  return <AdminClient />;
-}
