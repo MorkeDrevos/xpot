b35f1b1a2cb7a5e8de3b37519825ba889a1a5182
Mørke Drevos <drtseo@proton.me>
2025-12-13 20:54:55 +0100

    Update XpotTopBar.tsx

--- components/XpotTopBar.tsx
+++ components/XpotTopBar.tsx
@@ -9,21 +9,16 @@ import XpotSignInModal from './XpotSignInModal';
 export default function XpotTopBar() {
   return (
     <header className="relative border-b border-white/10 bg-black/75 backdrop-blur-md">
-      {/* Fixed header height */}
       <div className="mx-auto relative h-[132px] max-w-[1440px] px-4 sm:px-6">
-        {/* LOGO – visually centered in black bar */}
+        {/* Logo: visually centered in black bar, slightly lower to avoid purple banner influence */}
         <Link
           href="/"
-          className="absolute left-4 sm:left-6 top-[52%] -translate-y-1/2"
+          className="absolute left-4 sm:left-6 top-[58%] -translate-y-1/2"
         >
-          <XpotLogoLottie
-            width={520}
-            height={150}
-            mode="full"
-          />
+          <XpotLogoLottie width={520} height={150} mode="full" />
         </Link>
 
-        {/* RIGHT CONTROLS */}
+        {/* Right controls */}
         <div className="absolute right-4 sm:right-6 top-1/2 -translate-y-1/2 flex items-center gap-3">
           <ThemeToggle />
           <XpotSignInModal />
