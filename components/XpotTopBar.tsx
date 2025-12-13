5859bc5a70dab37ca7fa4f6b0a38cf7157a01816
Mørke Drevos <drtseo@proton.me>
2025-12-13 20:20:29 +0100

    Update XpotTopBar.tsx

--- components/XpotTopBar.tsx
+++ components/XpotTopBar.tsx
@@ -9,11 +9,11 @@ import XpotSignInModal from './XpotSignInModal';
 export default function XpotTopBar() {
   return (
     <header className="border-b border-white/10 bg-black/70 backdrop-blur-md">
-      <div className="mx-auto flex h-[76px] max-w-[1440px] items-center justify-between px-4 sm:px-6">
+      <div className="mx-auto flex h-[92px] max-w-[1440px] items-center justify-between px-4 sm:px-6">
         <Link href="/" className="flex items-center">
           <XpotLogoLottie
-            width={260}
-            height={72}
+            width={320}
+            height={88}
             mode="full"
             className="relative"
           />
