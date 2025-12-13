da01279e21e79cc2138554642e4610d0c6992f4c
Mørke Drevos <drtseo@proton.me>
2025-12-13 22:27:14 +0100

    Update XpotTopBar.tsx

--- components/XpotTopBar.tsx
+++ components/XpotTopBar.tsx
@@ -9,18 +9,16 @@ import XpotSignInModal from './XpotSignInModal';
 export default function XpotTopBar() {
   return (
     <header className="border-b border-white/10 bg-black/75 backdrop-blur-md">
-      <div className="mx-auto flex h-[104px] max-w-[1440px] items-center justify-between px-6">
-        {/* LEFT: Logo aligned to grid */}
-        <div className="flex items-center">
-          <Link href="/" className="inline-flex items-center">
-            {/* Prevent the logo from affecting vertical alignment */}
-            <div className="flex items-center leading-none">
-              <XpotLogoLottie width={460} height={120} />
-            </div>
-          </Link>
-        </div>
+      <div className="mx-auto flex h-[104px] w-full max-w-[1440px] items-center justify-between px-4 sm:px-6">
+        {/* LEFT: Logo aligned with page content */}
+        <Link href="/" className="inline-flex items-center">
+          {/* Tiny nudge to counter transparent padding in the PNG */}
+          <div className="-ml-1 flex items-center leading-none">
+            <XpotLogoLottie width={420} height={110} />
+          </div>
+        </Link>
 
-        {/* RIGHT: Controls aligned */}
+        {/* RIGHT: Controls */}
         <div className="flex items-center gap-3">
           <ThemeToggle />
           <XpotSignInModal />
