4ebac16eaca9f4a4734ed7ec6c24ff214e307d44
Mørke Drevos <drtseo@proton.me>
2025-12-13 22:06:08 +0100

    Create page.tsx

---
+++ app/hub/page.tsx
@@ -0,0 +1,10 @@
+// app/dashboard/page.tsx
+
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
+import DashboardClient from './DashboardClient';
+
+export default function DashboardPage() {
+  return <DashboardClient />;
+}
