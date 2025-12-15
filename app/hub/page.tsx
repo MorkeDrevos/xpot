45150b2ea5eae63239f158c99b4222c42cd6ad89
Mørke Drevos <drtseo@proton.me>
2025-12-15 11:08:56 +0100

    Update page.tsx

--- app/hub/page.tsx
+++ app/hub/page.tsx
@@ -2,15 +2,17 @@
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
-import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 import DashboardClient from './DashboardClient';
 
 export default function DashboardPage() {
   const { userId } = auth();
 
-  if (!userId) redirect('/sign-in');
+  if (!userId) {
+    redirect('/sign-in?redirect_url=/hub');
+  }
 
   return <DashboardClient />;
 }
