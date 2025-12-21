55edfa27de35b81939e5748469f05181550c91f7
Mørke Drevos <drtseo@proton.me>
2025-12-21 02:09:18 +0100

    Update xpotVaults.ts

--- lib/xpotVaults.ts
+++ lib/xpotVaults.ts
@@ -13,10 +13,10 @@ export type XpotVault = { name: string; address: string };
 
 export const XPOT_VAULTS: Record<XpotVaultKey, XpotVault[]> = {
   rewards: [
-    { name: 'Rewards / Distribution Vault', address: '4nsLrcTCVUjYuemcGAs26ySw7teRyofq2vzqSZiFukPJ' },
+    { name: 'XPOT Rewards / Distribution Vault', address: '8FfoRtXDj1Q1Y2DbY2b8Rp5bLBLLstd6fYe2GcDTMg9o' },
   ],
   liquidityOps: [
-    { name: 'Liquidity + Market Ops Vault', address: '4nsLrcTCVUjYuemcGAs26ySw7teRyofq2vzqSZiFukPJ' },
+    { name: 'Liquidity + Market Ops Vault', address: 'D2N1rt1N9fKqefQWpu5GH7N6r6H3uQQoaYksUUtkHcJR' },
   ],
   treasury: [
     { name: 'Treasury Vault', address: '4nsLrcTCVUjYuemcGAs26ySw7teRyofq2vzqSZiFukPJ' },
