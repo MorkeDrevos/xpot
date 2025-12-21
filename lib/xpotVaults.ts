449a7f93d6e644c50070b284b77e883d442f4558
Mørke Drevos <drtseo@proton.me>
2025-12-21 12:53:15 +0100

    Update xpotVaults.ts

--- lib/xpotVaults.ts
+++ lib/xpotVaults.ts
@@ -19,18 +19,18 @@ export const XPOT_VAULTS: Record<XpotVaultKey, XpotVault[]> = {
     { name: 'XPOT Liquidity + Market Ops Vault', address: '8VPiF9Mp8eoxSxg1m5m371d6D4V32iH6wf9KyMisxr67' },
   ],
   treasury: [
-    { name: 'Treasury Vault', address: '88QKK3TKdqoBVcYqHN3pevwAXVDm3bMp6G4NASuQ9Ean' },
+    { name: 'XPOT Treasury Vault', address: '88QKK3TKdqoBVcYqHN3pevwAXVDm3bMp6G4NASuQ9Ean' },
   ],
   team: [
-    { name: 'Team Vesting Vault', address: 'G17RehqUAgMcAxcnLUZyf6WzuPqsM82q9SC1aSkBUR7w' },
+    { name: 'XPOT Team Vesting Vault', address: 'G17RehqUAgMcAxcnLUZyf6WzuPqsM82q9SC1aSkBUR7w' },
   ],
   partners: [
-    { name: 'Partners + Creators Vault', address: '9wrsrr17nsMDU4oD3VG9hUB7supWvoPBWGxhG3BQmVV2' },
+    { name: 'XPOT Partners + Creators Vault', address: '9wrsrr17nsMDU4oD3VG9hUB7supWvoPBWGxhG3BQmVV2' },
   ],
   community: [
-    { name: 'Community Incentives Vault', address: 'Cn4GdKEKEiWh4UH3VNVrRkYHcgeYynThhP175iM9z2cc' },
+    { name: 'XPOT Community Incentives Vault', address: 'Cn4GdKEKEiWh4UH3VNVrRkYHcgeYynThhP175iM9z2cc' },
   ],
   strategic: [
-    { name: 'Strategic Reserve Vault', address: '8M1xt2PNEJkSrN6JZU7TGf7cGvL9Tp4W7obDy4Y7awx8' },
+    { name: 'XPOT Strategic Reserve Vault', address: '8M1xt2PNEJkSrN6JZU7TGf7cGvL9Tp4W7obDy4Y7awx8' },
   ],
 };
