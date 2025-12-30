92fd1090c3834eab1c09e81896fd80e57705fb80
Mørke Drevos <drtseo@proton.me>
2025-12-30 17:31:47 +0100

    Update XpotTopBar.tsx

--- components/XpotTopBar.tsx
+++ components/XpotTopBar.tsx
@@ -512,7 +512,9 @@ function PublicNavCenter({
 
       {/* Final Draw (primary) */}
       <NavPill href={FINAL_DAY_HREF} title={FINAL_DAY_LABEL}>
-        <Hourglass className="h-[15px] w-[15px] text-amber-200" />
+        <Hourglass
+  className="h-4 w-4 !text-white !stroke-white"
+/>
         <span className="tracking-wide">{FINAL_DAY_LABEL}</span>
       </NavPill>
 
